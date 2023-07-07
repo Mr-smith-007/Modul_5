e12dbc1883454ad2f4335cb4731e65b973e77567
Repnikov.A <repnikov.a@florcom.ru>
2023-07-07 11:08:23 +0300

    ShowColor с входящими параметрами

--- Modul_5/Program.cs
+++ Modul_5/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 class Programm
 {
-    static void ShowColor()
+    static string ShowColor(string username)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        Console.WriteLine($"{username} напишите свой любимый цвет на английском с маленькой буквы");
         var color = Console.ReadLine();
 
         switch (color)
@@ -34,25 +34,46 @@ class Programm
                 Console.WriteLine("Your color is yellow!");
                 break;
         }
+        return color;
     }
 
 
     public static void Main(string[] args)
     {
-        var (name, age) = ("Евгения", 27);
+        //var (name, age) = ("Евгения", 27);
 
-        Console.WriteLine("Мое имя: {0}", name);
-        Console.WriteLine("Мой возраст: {0}", age);
+        //Console.WriteLine("Мое имя: {0}", name);
+        //Console.WriteLine("Мой возраст: {0}", age);
+
+        //Console.Write("Введите имя: ");
+        //name = Console.ReadLine();
+        //Console.Write("Введите возрас с цифрами:");
+        //age = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("Ваше имя: {0}", name);
+        //Console.WriteLine("Ваш возраст: {0}", age);
+
+        (string name, int age) anketa;
 
         Console.Write("Введите имя: ");
-        name = Console.ReadLine();
-        Console.Write("Введите возрас с цифрами:");
-        age = Convert.ToInt32(Console.ReadLine());
+        anketa.name = Console.ReadLine();
+        Console.Write("Введите возраст с цифрами: ");
+        anketa.age = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Ваше имя: {0}", name);
-        Console.WriteLine("Ваш возраст: {0}", age);
+        Console.WriteLine("Ваше имя: {0}", anketa.name);
+        Console.WriteLine("Ваш возраст: {0}", anketa.age);
 
-        ShowColor();
+        //Console.ReadKey();
+
+        var favcolors = new string[3];
+
+        for (int i = 0; i < favcolors.Length; i++)
+        {
+            favcolors[i] = ShowColor(anketa.name);
+        }
+
+
+
 
 
     }
