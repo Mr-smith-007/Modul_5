7db1c3fe8897eb5893bd1f0556359f92b6003bb2
Repnikov.A <repnikov.a@florcom.ru>
2023-07-07 09:43:34 +0300

    Задание 1

--- Modul_5/Program.cs
+++ Modul_5/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+        (string Name, string[] Dishes) User;
+        Console.Write("Введите имя пользователя: ");
+        User.Name = Console.ReadLine();
+        User.Dishes = new string[5];
+        Console.WriteLine("Введите 5 любимых блюд:");
+        foreach (string x in User.Dishes)
+        {
+            Console.ReadLine();
+        }
+
+
+    }
+}
