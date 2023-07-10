00d752024633336c04aca0b8b6e8d6b839d79f28
Repnikov.A <repnikov.a@florcom.ru>
2023-07-10 14:10:53 +0300

    Задание 5.2.17

--- Modul_5/Program.cs
+++ Modul_5/Program.cs
@@ -1,47 +1,42 @@
 ﻿using System;
 class Programm
 {
-    //static string ShowColor(string username, int userage)
-    //{
-    //    Console.WriteLine($"{username}, {userage} лет \nНапишите свой любимый цвет на английском с маленькой буквы");
-    //    var color = Console.ReadLine();
+    static string ShowColor(string username, int userage)
+    {
+        Console.WriteLine($"{username}, {userage} лет \nНапишите свой любимый цвет на английском с маленькой буквы");
+        var color = Console.ReadLine();
 
-    //    switch (color)
-    //    {
-    //        case "red":
-    //            Console.BackgroundColor = ConsoleColor.Red;
-    //            Console.ForegroundColor = ConsoleColor.Black;
-
-    //            Console.WriteLine("Your color is red!");
-    //            break;
-
-    //        case "green":
-    //            Console.BackgroundColor = ConsoleColor.Green;
-    //            Console.ForegroundColor = ConsoleColor.Black;
-
-    //            Console.WriteLine("Your color is green!");
-    //            break;
-    //        case "cyan":
-    //            Console.BackgroundColor = ConsoleColor.Cyan;
-    //            Console.ForegroundColor = ConsoleColor.Black;
-
-    //            Console.WriteLine("Your color is cyan!");
-    //            break;
-    //        default:
-    //            Console.BackgroundColor = ConsoleColor.Yellow;
-    //            Console.ForegroundColor = ConsoleColor.Red;
-
-    //            Console.WriteLine("Your color is yellow!");
-    //            break;
-    //    }
-    //    return color;
-    //}
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+                break;
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+        return color;
+    }
 
     static int[] GetArrayFromConsole(int num = 5)
     {
         int[] array = new int[num];
         Console.WriteLine("Введите элементы массива:");
-        for(int i = 0; i < array.Length; i++)
+        for (int i = 0; i < array.Length; i++)
         {
             array[i] = int.Parse(Console.ReadLine());
         }
@@ -53,7 +48,7 @@ class Programm
         int t;
         for (int i = 0; i < array.Length; i++)
         {
-            for (int j = i + 1; j < array.Length; j++ )
+            for (int j = i + 1; j < array.Length; j++)
             {
                 if (array[i] > array[j])
                 {
@@ -65,8 +60,18 @@ class Programm
         }
         return array;
     }
-    
 
+    static void ShowArray(int[] array, bool sort = false)
+    {
+        if (sort)
+        {
+            SortArray(array);
+        }
+        foreach (int x in array)
+        {
+            Console.Write(x + " ");
+        }
+    }
 
 
     public static void Main(string[] args)
@@ -96,16 +101,25 @@ class Programm
 
         ////Console.ReadKey();
 
-        //var favcolors = new string[3];
 
+        //var favcolors = new string[3];
         //for (int i = 0; i < favcolors.Length; i++)
         //{
         //    favcolors[i] = ShowColor(anketa.name, anketa.age);
         //}
 
+
+
         int[] array = GetArrayFromConsole();
-        int[] sortedarray = SortArray(array);
-        foreach (int x in sortedarray)
-            Console.Write(x + " ");
+        ShowArray(array, true);
+
+        //int[] sortedarray = SortArray(array);
+        //foreach (int x in sortedarray)
+        //    Console.Write(x + " ");
+
+
+
+
+
     }
 }
